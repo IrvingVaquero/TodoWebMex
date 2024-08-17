d8b3a53677c446584ed47a45635620e8795e182e
admin.irving.vaquero <irving.vaquero@coficab.com>
2024-08-16 20:51:52 -0600

    Diseno de aplicacion

---
+++ ToDo_WebMex/Controllers/Home/TaskManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo_WebMex.Controllers.Home
+{
+    public class TaskManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Home/TaskManager/Index.cshtml");
+        }
+    }
+}
